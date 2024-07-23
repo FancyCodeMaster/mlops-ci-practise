606af3fa06a2a2cb4bb35bb9688768983252ad6e
Rikesh Silwal Khatri <53807517+FancyCodeMaster@users.noreply.github.com>
2024-07-23 08:12:34 +0545

    Update model.py

--- model.py
+++ model.py
@@ -135,7 +135,16 @@ regressor.fit(X_train , y_train)
 y_pred = regressor.predict(X_test)
 
 from sklearn.metrics import classification_report
-print(classification_report(y_test, y_pred, target_names=train.columns[0:-1]))
+y_true = [0, 1, 2, 2, 2]
+y_pred = [0, 0, 2, 2, 1]
+target_names = ['class 0', 'class 1', 'class 2']
+print(classification_report(y_true, y_pred, target_names=target_names))
+
+
+with open('random-test-metrics.txt', 'w') as f:
+    f.write(classification_report(y_true, y_pred, target_names=target_names))
+
+
 
 
 
